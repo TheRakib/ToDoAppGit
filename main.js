bfc37e573c829d4883395a158709e7a58b751658
feliciatranberg <felicia@substansgbg.se>
2021-02-02 22:41:45 +0100

    add route and oraginase

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+// const bodyParser = require("body-parser");
 const sassMiddleware = require('node-sass-middleware');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-
-const TodoTask = require("./models/TodoTask");
+const router = require('./routes/todoRoute');
+const TodoTask = require('./models/TodoTask');
 
 dotenv.config();
 
@@ -14,62 +15,14 @@ app.use(sassMiddleware({
     dest: __dirname + '/public/stylesheets/', 
     outputStyle: 'compressed' 
   }),
- 
 )
-app.use(express.static(path.join(__dirname, 'public'))) 
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/static", express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
-
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", router);
 app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-    TodoTask.find({}, (err, tasks) => {
-    res.render("todo.ejs", { todoTasks: tasks });
-    });
-    });
-
-    //  input test
-    // app.post('/',(req, res) => {
-    //     console.log(req.body);
-    //     });
-
-        app.post('/',async (req, res) => {
-            const todoTask = new TodoTask({
-            content: req.body.content
-            });
-            try {
-            await todoTask.save();
-            res.redirect("/");
-            } catch (err) {
-            res.redirect("/");
-            }
-            });
-
-app
-.route("/edit/:id")
-.get((req, res) => {
-const id = req.params.id;
-TodoTask.find({}, (err, tasks) => {
-res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
-});
-})
-.post((req, res) => {
-const id = req.params.id;
-TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
-if (err) return res.send(500, err);
-res.redirect("/");
-});
-});
-
-app.route("/remove/:id").get((req, res) => {
-    const id = req.params.id;
-    TodoTask.findByIdAndRemove(id, err => {
-    if (err) return res.send(500, err);
-    res.redirect("/");
-    });
-    });
 
         mongoose.set("useFindAndModify", false);
         mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
