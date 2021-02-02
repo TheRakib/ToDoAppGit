6db0df09abbde9eb549c21a4aa0d70132d7aa26e
feliciatranberg <felicia@substansgbg.se>
2021-02-02 15:45:08 +0100

    testing input with post metod

--- main.js
+++ main.js
@@ -14,10 +14,17 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/static", express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.get('/',(req, res) => {
     res.render('todo.ejs');
     });
+
+    // input test
+    app.post('/',(req, res) => {
+        console.log(req.body);
+        });
 
 app.listen(7700, () => console.log("Servern funkar!"));
